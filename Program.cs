ea4895af88b4f43f07cf60f222c602f2faaa6e9e
vakho <vaxo.gr2@gmail.com>
2024-12-01 19:44:39 +0400

    if statement

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 // #1 Sting Methods
-// String fullName = "Bro Code";
-// String phoneNumber = "598-91-10-07";
-// String forTrime = "                Bro Code                  ";
+// String fullName = "Vakho Cloud";
+// String phoneNumber = "598-00-00-00";
+// String forTrime = "                Vakho Cloud                  ";
 
 // fullName = fullName.ToUpper();
 // fullName = fullName.ToLower();
@@ -18,8 +18,8 @@
 
 // Console.WriteLine(fullName.Length);
 
-// String firstName = fullName.Substring(0, 3);
-// String lastName = fullName.Substring(4, 4);
+// String firstName = fullName.Substring(0, 5);
+// String lastName = fullName.Substring(6, 5);
 
 // Console.WriteLine("First name : " + firstName);
 // Console.WriteLine("Last name : " + lastName);
@@ -30,8 +30,15 @@
 Console.WriteLine("Please enter your age: ");
 int age = Convert.ToInt32(Console.ReadLine());
 
-if(age > 18) {
+if(age > 18) 
+{
     Console.WriteLine("You are signed up!");
-}else { 
+}
+else if(age < 0)
+{
+    Console.WriteLine("You haven't been born yeat");
+}
+else    
+{ 
     Console.WriteLine("You are " + age);
 }
